af2816004c6d15ded6347d8f73bb13b19c5b68bc
GByteos <gbyte@gb-tronix.de>
2023-05-04 21:11:05 +0200

    Implemented bas viewe model with classic INotifyPropertyChanged

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -1,5 +1,45 @@
-﻿namespace MonkeyFinder.ViewModel;
+﻿using Xamarin.Google.Crypto.Tink.Signature;
 
-public class BaseViewModel
+namespace MonkeyFinder.ViewModel;
+
+public class BaseViewModel : INotifyPropertyChanged
 {
+    bool isBusy;
+    string title;
+
+    public bool IsBusy
+    {
+        get => isBusy;
+        set
+        {
+            if (isBusy == value)
+                return;
+
+            isBusy = value;
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(IsNotBusy));
+        }
+    }
+
+    public string Title
+    {
+        get => title;
+        set
+        {
+            if (title == value) 
+                return;
+
+            title = value;
+            OnPropertyChanged(nameof(Title));
+        }
+    }
+
+    public bool IsNotBusy => !isBusy;
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public void OnPropertyChanged([CallerMemberName]string name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
 }
