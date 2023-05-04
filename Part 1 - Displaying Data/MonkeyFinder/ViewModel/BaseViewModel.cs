576f7df4c8c8101ed45df6700a7095d3ac8c377c
GByteos <gbyte@gb-tronix.de>
2023-05-04 21:19:29 +0200

    Changed it to use MVVM COM TK

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -1,45 +1,20 @@
-﻿using Xamarin.Google.Crypto.Tink.Signature;
-
+﻿
 namespace MonkeyFinder.ViewModel;
 
-public class BaseViewModel : INotifyPropertyChanged
+public partial class BaseViewModel : ObservableObject
 {
+    public BaseViewModel() 
+    {
+        
+    }
+
+    [ObservableProperty]
     bool isBusy;
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsNotBusy))]
     string title;
 
-    public bool IsBusy
-    {
-        get => isBusy;
-        set
-        {
-            if (isBusy == value)
-                return;
-
-            isBusy = value;
-            OnPropertyChanged();
-            OnPropertyChanged(nameof(IsNotBusy));
-        }
-    }
-
-    public string Title
-    {
-        get => title;
-        set
-        {
-            if (title == value) 
-                return;
-
-            title = value;
-            OnPropertyChanged(nameof(Title));
-        }
-    }
 
     public bool IsNotBusy => !isBusy;
-
-    public event PropertyChangedEventHandler PropertyChanged;
-
-    public void OnPropertyChanged([CallerMemberName]string name = null)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-    }
 }
